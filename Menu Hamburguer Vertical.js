df9d9664f11646e9ff4f6828122c688c03a3e14e
=Goza de Farinha <odamasenganumeko@gmail.com>
2023-05-31 23:06:09 -0300

    Versão Final do Menu Hamburguer (JavaScript)

---
+++ Menu Hamburguer Vertical.js
@@ -0,0 +1,12 @@
+function abreMenu()  /*Alterar a altura do Menu Hamburguer para 100%*/
+{
+  document.getElementById("MenuMobile").style.height  = "100%";
+
+}
+
+function fechaMenu()  /*Alterar a altura do Menu Hamburguer para 0%*/
+{
+  document.getElementById("MenuMobile").style.height = "0%";
+
+  
+}
