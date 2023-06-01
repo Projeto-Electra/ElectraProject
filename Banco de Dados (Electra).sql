bc8a6b0599dabc351386226cbd1e2626f522da9c
=Goza de Farinha <odamasenganumeko@gmail.com>
2023-05-31 23:00:29 -0300

    Versão 1.4 Final do Banco de Dados(MySQL) da Electra, versão corrigida conforme modelos Lógico e Conceitual

--- Banco de Dados (Electra).sql
+++ Banco de Dados (Electra).sql
@@ -19,8 +19,7 @@ CódigoContato int Primary Key auto_increment,
 TelefoneCliente varchar(150) not null,
 EmailCliente varchar(200) not null,
 CódigoClienteConta int not null,
-foreign key(CódigoClienteConta) references Cliente(CódigoCliente),
-EndereçoCliente varchar(200) not null
+foreign key(CódigoClienteConta) references Cliente(CódigoCliente)
 
 );
 
@@ -53,9 +52,8 @@ ValorVenda double(9,3) not null,
 DataVenda date not null,
 CódigoPagamento int not null,
 foreign key(CódigoPagamento) references Pagamento(CódigoPagamento),
-QuantidadeVenda int not null,
-CódigoEstoqueVenda int not null,
-foreign key(CódigoEstoqueVenda) references Estoque(CódigoEstoque)
+QuantidadeVenda int not null
+
 
 );
 
@@ -72,12 +70,25 @@ foreign key(CódigoEstoqueProd) references Estoque(CódigoEstoque)
 create table ProdutoVenda(
 
 CódigoProdutoVenda int Primary Key auto_increment,
-CódigoVenda int not null,
-foreign key(CódigoVenda) references Venda(CódigoVenda),
-CódigoProduto int not null,
-foreign key(CódigoProduto) references Produto(CódigoProduto)
+CódigoVendaProd int not null,
+foreign key(CódigoVendaProd) references Venda(CódigoVenda),
+CódigoProdutoVend int not null,
+foreign key(CódigoProdutoVend) references Produto(CódigoProduto)
 
 );
+
+create table VendaEstoque
+(
+
+CódigoVendaEstoque  int Primary Key auto_increment,
+CódigoVendaEsto int not null,
+foreign key(CódigoVendaEsto) references Venda(CódigoVenda),
+CódigoEstoqueVend int not null,
+foreign key(CódigoEstoqueVend ) references Estoque(CódigoEstoque)
+
+);
+
+
 
 /*Inserindo nas Tabelas*/
 
@@ -86,11 +97,11 @@ Values("Minions 2", 343545,"maça123"),
 	  ("Shrek 4", 456788,"banana123"),
       ("Manoel Gomes", 343545,"manga123");
       
-insert into Contato(CódigoClienteConta, TelefoneCliente, EmailCliente, EndereçoCliente)
-Values(1, "(21)95423-5567","Minions@gmail.com","AV. Mem de Sá, RJ "),
-	  (2, "(11)98642-4356","Shrek@gmail.com","De baixo da Ponte, SP"),
-      (3, "(81)90853-4356","Gomes@gmail.com","Caruaru, PE"),
-	  (2, "(11)92632-4326","ShrekAlt@hotmail.com","De baixo da Ponte, SP");
+insert into Contato(CódigoClienteConta, TelefoneCliente, EmailCliente)
+Values(1, "(21)95423-5567","Minions@gmail.com"),
+	  (2, "(11)98642-4356","Shrek@gmail.com"),
+      (3, "(81)90853-4356","Gomes@gmail.com"),
+	  (2, "(11)92632-4326","ShrekAlt@hotmail.com");
 
 insert into Estoque(QuantidadeEstoque, Descrição, Seção)
 Values(10,"Motos Elétricas da GWS","GWS"),
@@ -108,20 +119,25 @@ values(3,91.980,str_to_date('20/09/2023', '%d/%m/%Y' ),0),
       (1,133.000,str_to_date('12/03/2023', '%d/%m/%Y' ),0),
        (2,14.900,str_to_date('23/05/2023', '%d/%m/%Y' ),0);
       
-insert into Venda(QuantidadeVenda, CódigoEstoqueVenda, ValorVenda, DataVenda, CódigoPagamento)
-values(2,"2", '45.990',STR_TO_DATE('21/03/2021', '%d/%m/%Y') ,1),
-      (1,"1", 133.000, str_to_date('12/03/2023', '%d/%m/%Y' ),3),
-      (4,"3", 19.990, str_to_date('21/05/2022', '%d/%m/%Y' ),2),
-      (1,"3", '19.990',STR_TO_DATE('22/05/2023', '%d/%m/%Y') ,4);
+insert into Venda(QuantidadeVenda, ValorVenda, DataVenda, CódigoPagamento)
+values(2,'45.990',STR_TO_DATE('21/03/2021', '%d/%m/%Y') ,1),
+      (1, 133.000, str_to_date('12/03/2023', '%d/%m/%Y' ),3),
+      (4, 19.990, str_to_date('21/05/2022', '%d/%m/%Y' ),2),
+      (1, '19.990',STR_TO_DATE('22/05/2023', '%d/%m/%Y') ,4);
 
-insert into ProdutoVenda(CódigoProduto, CódigoVenda)
+insert into ProdutoVenda(CódigoProdutoVend, CódigoVendaProd)
+values(1,3),
+      (2,1),
+      (3,2);
+      
+insert into VendaEstoque(CódigoVendaEsto,CódigoEstoqueVend)
 values(1,3),
       (2,1),
       (3,2);
 
 
 /*Inner Join para Consultar os Contatos dos Clientes*/
-Select Nome, CPF, TelefoneCliente, EmailCliente, EndereçoCliente from Contato
+Select Nome, CPF, TelefoneCliente, EmailCliente from Contato
 Inner Join Cliente
 On(Cliente.CódigoCliente = Contato.CódigoClienteConta);
 
@@ -132,14 +148,17 @@ Inner Join Pagamento
 On(Cliente.CódigoCliente= Pagamento.CódigoClienteContaPag);
 
 /*Inner Join para Consultar o Estoque dos Produtos*/
-Select NomeProduto, Preço, Marca, QuantidadeEstoque,Descrição, Seção from Produto
-Inner Join Estoque
-On(Produto.CódigoEstoqueProd = Estoque.CódigoEstoque);
+Select NomeProduto, Preço, Marca, QuantidadeEstoque,Descrição, Seção from Estoque
+Inner Join Produto
+On(Estoque.CódigoEstoque = Produto.CódigoEstoqueProd);
+
 
 /*Inner Join para Consultar os Produtos que foram Vendidos*/
-Select NomeProduto, Preço, Marca,(ValorVenda * QuantidadeVenda) as  ValorVenda,DataVenda,QuantidadeVenda from Produto
+Select NomeProduto, Preço, Marca,(ValorVenda * QuantidadeVenda) as  ValorVenda,DataVenda,QuantidadeVenda from ProdutoVenda
 Inner join Venda
-On(Produto.CódigoEstoqueProd = Venda.CódigoEstoqueVenda);
+On(ProdutoVenda.CódigoVendaProd = Venda.CódigoVenda)
+Inner join Produto
+On(Produto.CódigoProduto = ProdutoVenda.CódigoProdutoVend );
 
 /*Inner Join para Consultar quanto cada Cliente deve */
 SELECT Cliente.Nome, Cliente.CPF, Venda.QuantidadeVenda, Venda.ValorVenda * Venda.QuantidadeVenda as ValorVenda,  DataVenda, Pagamento.ValorPago, Venda.ValorVenda * Venda.QuantidadeVenda - Pagamento.ValorPago AS Divida, Pagamento.Datapag, Pagamento.Desconto
@@ -155,6 +174,8 @@ INNER JOIN Venda
 ON (Venda.CódigoPagamento = Pagamento.CódigoPagamento);
 
 /*inner Join para Consultar se tem o Estoque necessário pra Venda */
-Select QuantidadeVenda,ValorVenda * QuantidadeVenda as ValorVenda, DataVenda, QuantidadeEstoque, QuantidadeEstoque - QuantidadeVenda as EstoquePósVenda,Descrição, Seção from Venda
+Select QuantidadeVenda,ValorVenda * QuantidadeVenda as ValorVenda, DataVenda, QuantidadeEstoque, QuantidadeEstoque - QuantidadeVenda as EstoquePósVenda,Descrição, Seção from VendaEstoque
+Inner Join Venda
+On(Venda.CódigoVenda = VendaEstoque.CódigoVendaEsto)
 Inner Join Estoque
-On(Estoque.CódigoEstoque = Venda.CódigoEstoqueVenda);
+On(Estoque.CódigoEstoque = VendaEstoque.CódigoEstoqueVend);
